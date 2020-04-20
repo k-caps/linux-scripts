5b8055d75956153977c08e44256e0eb9a736622b
Kobi <kobirosenstein@gmail.com>
2020-04-20 18:57:58 +0300

    Update install_vim_and_bash_powerline.sh

--- install_vim_and_bash_powerline.sh
+++ install_vim_and_bash_powerline.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Hack font:
-# (Modern distros can often install the .ttf file simply by doubleclicking it)
+# If the script fails to install the font you can just download and install it manually from the link
+  # (Modern distros can often install the .ttf file simply by doubleclicking it)
 wget https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf
 mkdir -p ~/.fonts
 mv 'Hack Regular Nerd Font Complete.ttf' ~/.fonts/
 fc-cache -fv ~/.fonts/
 
-# Now you neeeed to update your terminal app to use this font called "Hack Regular"
+# Now you neeeed to update your terminal app to use this font that we just installed, called "Hack Regular"
 # packages UBUNTU ONLY:
 sudo apt update
 sudo apt-get install python-pip git powerline
@@ -37,3 +38,5 @@ set laststatus=2
 " Use 256 colours (Use this setting only if your terminal supports 256 colours)
 set t_Co=25
 EOF
+
+source ~/.bashrc
