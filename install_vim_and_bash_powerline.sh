d33434e031a57c589619bba4b305d47258b29ca1
Kobi <kobirosenstein@gmail.com>
2020-04-20 18:41:57 +0300

    Update install_vim_and_bash_powerline.sh

--- install_vim_and_bash_powerline.sh
+++ install_vim_and_bash_powerline.sh
@@ -7,7 +7,6 @@ mkdir -p ~/.fonts
 mv 'Hack Regular Nerd Font Complete.ttf' ~/.fonts/
 fc-cache -fv
 
-
 # Now you neeeed to update your terminal app to use this font called "Hack Regular"
 # packages UBUNTU ONLY:
 sudo apt update
@@ -15,17 +14,21 @@ sudo apt-get install python-pip git powerline
 pip install --user git+git://github.com/Lokaltog/powerline
 # bash:
 cat >> ~/.bashrc << EOF
+
 if [ -f `which powerline-daemon` ]; then
   powerline-daemon -q
   POWERLINE_BASH_CONTINUATION=1
   POWERLINE_BASH_SELECT=1
   . /usr/share/powerline/bindings/bash/powerline.sh
 fi
-
 EOF
 
 # vim:
 cat >> /etc/vim/vimrc << EOF
+
+python3 from powerline.vim import setup as powerline_setup
+python3 powerline_setup()
+python3 del powerline_setup
 set rtp+=$HOME/.local/lib/python2.7/site-packages/powerline/bindings/vim/
 
 " Always show statusline
